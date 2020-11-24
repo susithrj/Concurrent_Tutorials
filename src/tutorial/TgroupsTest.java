e631575818b4252332b9e5c743eab0fc48275850
susithrj <susithlion@gmail.com>
2020-11-24 10:12:14 +0530

    tgroup test.

---
+++ src/tutorial/TgroupsTest.java
@@ -0,0 +1,13 @@
+package tutorial;
+
+public class TgroupsTest extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("tgroup test");
+    }
+}
